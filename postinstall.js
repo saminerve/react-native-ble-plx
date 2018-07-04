8cff401cd1145f3504c52f9ff53b96cf00b6d44f
Sami Maaref <maaref.sami@gmail.com>
2018-07-04 15:17:32 +0200

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -12,30 +12,36 @@ try {
   console.warn('You should run react-native-ble-plx from root of your project')
 }
 
+console.warn('===VISEO=== A')
 if (process.platform === 'darwin' && shouldUseCarthage()) {
+  console.warn('===VISEO=== B')
   var carthageVersionProcessResult = childProcess.spawnSync('carthage', ['version'], {
     stdio: 'pipe'
   })
-
+console.warn('===VISEO=== C')
   if (carthageVersionProcessResult.status != 0) {
     // `carthage` not found (probably)
     errorExitProcess(
       'carthage is required to compile frameworks for iOS backend. You can install it with brew: "brew install carthage". After installation go to ./node_modules/react-native-ble-plx and run "./postinstall.js" or reinstall node module'
     )
   }
-
+console.warn('===VISEO=== E')
   const bleClientManagerDirectory = __dirname + '/ios/BleClientManager'
   try {
+    console.warn('===VISEO=== F')
     process.chdir(bleClientManagerDirectory)
+    console.warn('===VISEO=== G')
   } catch (err) {
     errorExitProcess(`${bleClientManagerDirectory} directory not found. Cannot proceed with building the library.`)
   }
-
+  console.warn('===VISEO=== H')
   spawnSyncProcessAndExitOnError('carthage', ['bootstrap', ...platformParams])
+  console.warn('===VISEO=== I')
 
   const carthageVersionString = carthageVersionProcessResult.output[1].toString()
+  
   spawnSyncProcessAndExitOnError('carthage', getCarthageBuildParams(carthageVersionString))
-
+console.warn('===VISEO=== J')
   process.exit(0)
 }
 
@@ -52,6 +58,7 @@ function errorExitProcess(errorMessage) {
 }
 
 function spawnSyncProcessAndExitOnError(command, params) {
+  console.warn('===VISEO=== command: '+command+' params: '+params)
   const result = childProcess.spawnSync(command, params, {
     stdio: 'inherit'
   })
